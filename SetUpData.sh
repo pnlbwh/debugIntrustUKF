de19889184e2768d5f4b3e95307a04d5626b5c3e
reckbo <reckbo@bwh.harvard.edu>
2016-10-27 22:23:46 +1100

    STY: remove old SetUpData.sh

--- SetUpData.sh
+++
@@ -1,6 +0,0 @@
-source $int/harmonized-newquery/SetUpData.sh
-
-matlengths=_data/tractlengths/$case.tractlengths.mat.txt
-vtklengths=_data/tractlengths/$case.tractlengths.vtk.txt
-fa=_data/FA-masked/$case.FA-masked.nrrd
-famask=_data/FA-mask/$case.FA-mask.nrrd
